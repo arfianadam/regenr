0c9bc03678789302cbed2a16a2341cba27492cb8
Arfian Adam <arfianadam@yahoo.com>
2017-07-21 14:09:48 +0700

    added eslint and applied some code writing tweak

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 #!/usr/bin/env node
-var program = require('commander'),
-	filendir = require('filendir'),
-	colors = require('colors'),
-	templates = require('./template.js');
+var program = require("commander"),
+  filendir = require("filendir"),
+  colors = require("colors"),
+  templates = require("./template.js");
 
 var createFile = function(path,css) {
-	css = css || 'scss'
+  css = css || "scss";
 
-	var filename = path.match(/[^\\|\/]*$/)[0]
-	var dir = path.substring((path.match(/[^\\|\/]*$/).index), -1)
+  var filename = path.match(/[^\\|/]*$/)[0];
+  var dir = path.substring((path.match(/[^\\|/]*$/).index), -1);
 
-	console.log(`\n=====================\n== React Generator ==\n=====================`)
+  console.log("\n=====================\n== React Generator ==\n=====================");
 
-	filendir.writeFile(dir + filename + "/" + filename + ".js", templates.getReact(filename, css, true) , function(err) {
-		if (err) return console.log(err)
+  filendir.writeFile(dir + filename + "/" + filename + ".js", templates.getReact(filename, css, true) , function(err) {
+    if (err) return console.log(err);
 
-		console.log("Created " + `${dir}${filename}/` + colors.yellow.underline(`${filename}.js`))
-		return true;
-	})
+    console.log("Created " + `${dir}${filename}/` + colors.yellow.underline(`${filename}.js`));
+    return true;
+  });
 
-	filendir.writeFile(`${dir}${filename}/${filename}.${css}`, templates.getStyle(filename, css) , function(err) {
-		if (err) return console.log(err)
-		console.log('Created ' + `${dir}${filename}/` + colors.blue.underline(`${filename}.${css}`))
-		return true;
-	})
+  filendir.writeFile(`${dir}${filename}/${filename}.${css}`, templates.getStyle(filename, css) , function(err) {
+    if (err) return console.log(err);
+    console.log("Created " + `${dir}${filename}/` + colors.blue.underline(`${filename}.${css}`));
+    return true;
+  });
 
-	filendir.writeFile(dir + filename + "/" + "package.json", templates.getJson(filename, true) , function(err) {
-		if (err) return console.log(err)
-		
-		console.log("Created " + `${dir}${filename}/` + colors.magenta.underline(`package.json`))
-		return true;
-	})
-}
+  filendir.writeFile(dir + filename + "/" + "package.json", templates.getJson(filename, true) , function(err) {
+    if (err) return console.log(err);
+    
+    console.log("Created " + `${dir}${filename}/` + colors.magenta.underline("package.json"));
+    return true;
+  });
+};
 
 program
-	.arguments('<file>')
-	.option('-c, --css <css>', 'Css preprocessor')
-	.action(function(file) {
-		if (typeof program.css != 'undefined') {
-			createFile(file, program.css)
-		} else {
-			createFile(file)
-		}
-	})
-	.parse(process.argv)
+  .arguments("<file>")
+  .option("-c, --css <css>", "Css preprocessor")
+  .action(function(file) {
+    if (typeof program.css != "undefined") {
+      createFile(file, program.css);
+    } else {
+      createFile(file);
+    }
+  })
+  .parse(process.argv);
--- template.js
+++ template.js
@@ -1,25 +1,24 @@
 var template = (() => {
   var options = {
-    css: 'scss',
+    css: "scss",
     privateRep: true,
     cssModules: false
-  }
+  };
 
   var getJson = (componentName, privateRep) => {
-    privateRep = privateRep || options.privateRep
-    
+    privateRep = privateRep || options.privateRep;
     return `{
   "name": "${componentName}",
   "version": "0.0.0",
   "private": ${privateRep},
   "main": "./${componentName}.js"
-}`
-  }
+}`;
+  };
 
   var getReact = (componentName, style, cssModules) => {
-    cssModules = cssModules || options.cssModules
+    cssModules = cssModules || options.cssModules;
     return `import React, { Component } from 'react';
-${cssModules ? "import styles from './" + componentName + '.' + style + "';" : ''}
+${cssModules ? "import styles from './" + componentName + "." + style + "';" : ""}
 
 export default class ${componentName} extends Component {
   constructor(props) {
@@ -29,33 +28,33 @@ export default class ${componentName} extends Component {
 
   render() {
     return (
-      <div ${cssModules ? 'className={' + 'styles.' + componentName + '}' : '' }>
+      <div ${cssModules ? "className={" + "styles." + componentName + "}" : "" }>
         {/* Your code here */}
       </div>
     );
   }
 }
-`
-  }
+`;
+  };
 
   var getStyle = (componentName,style) => {
-    style = style || 'scss'
+    style = style || "scss";
 
-    if (style === 'sass') {
+    if (style === "sass") {
       return `.${componentName}
-  /* Your stylesheet here */`
+  /* Your stylesheet here */`;
     } else {
       return `.${componentName} {
   /* Your stylesheet here */
-}`
+}`;
     }
-  }
+  };
 
   return {
     getStyle: getStyle,
     getReact: getReact,
     getJson: getJson
-  }
-})()
+  };
+})();
 
 module.exports = template;
